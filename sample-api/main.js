c4a31e5fa4bc8665f056258e7ada4536c8218e3a
Baatarvant <baatarvant@gmail.com>
2024-02-20 17:13:11 +0800

    post method

--- sample-api/main.js
+++ sample-api/main.js
@@ -4,13 +4,15 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/articles", (req, res) => {
   const data = fs.readFileSync("articles.json", "utf8");
   res.json(JSON.parse(data));
 });
 
-app.get("/articles/create", (req, res) => {
-  const { title, desc } = req.query;
+app.post("/articles/create", (req, res) => {
+  const { title, desc } = req.body;
 
   const data = fs.readFileSync("articles.json", "utf8");
   const list = JSON.parse(data);
@@ -19,9 +21,7 @@ app.get("/articles/create", (req, res) => {
     title: title,
     desc: desc,
   });
-
   fs.writeFileSync("articles.json", JSON.stringify(list));
-
   res.json([{ status: "Success" }]);
 });
 
