1acbfdd0a1006be74fbcac328c8d28f420a6c2cc
erdenetsogta <a.erdenetsogt@gmail.com>
2024-02-27 16:55:24 +0800

    sql crud

---
+++ sample-api/database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tasks (
+    id BIGINT PRIMARY KEY, title VARCHAR(255), description TEXT, createdAt DATE
+);
+
+select * from tasks
+
+select title, id from tasks
+
+select title from tasks
+
+insert into tasks values (5, 'Shalaa ugaa');
+
+insert into tasks values (6, 'Toosoo soruul');
+
+delete from tasks where id = 5;
+
+update tasks set title = 'Aygaa ugaa' where id = 6;
--- sample-api/main.js
+++ sample-api/main.js
@@ -29,48 +29,22 @@ app.get("/tasks", async (req, res) => {
   res.json(result);
 });
 
-app.post("/tasks/create", (req, res) => {
+app.post("/tasks/create", async (req, res) => {
   const { title } = req.body;
-
-  const data = fs.readFileSync("tasks.json", "utf8");
-  const list = JSON.parse(data);
-
-  const taskId = Date.now();
-
-  list.push({
-    id: taskId,
-    title: title,
-  });
-
-  fs.writeFileSync("tasks.json", JSON.stringify(list));
+  await sql`insert into tasks(id, title) values(${Date.now()}, ${title})`;
   res.json([{ status: "Success" }]);
 });
 
-app.put("/tasks/update/:id", (req, res) => {
+app.put("/tasks/update/:id", async (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-
-  const data = fs.readFileSync("tasks.json", "utf8");
-  const list = JSON.parse(data);
-
-  const index = list.findIndex((item) => item.id === Number(id));
-
-  list[index].title = title;
-  fs.writeFileSync("tasks.json", JSON.stringify(list));
-
+  await sql`update tasks set title = ${title} where id = ${id}`;
   res.json([{ status: "Success" }]);
 });
 
-app.delete("/tasks/delete/:id", (req, res) => {
+app.delete("/tasks/delete/:id", async (req, res) => {
   const { id } = req.params;
-
-  const data = fs.readFileSync("tasks.json", "utf8");
-  const list = JSON.parse(data);
-
-  const newList = list.filter((item) => item.id !== Number(id));
-
-  fs.writeFileSync("tasks.json", JSON.stringify(newList));
-
+  await sql`delete from tasks where id = ${id}`;
   res.json([{ status: "Success" }]);
 });
 
